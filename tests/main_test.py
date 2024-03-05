f8d08904e77626058de8408f0894efa76710b30c
Carlos Fuentes <educharlief@gmail.com>
2024-03-05 16:24:56 -0500

    Test ge (#4)

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,6 +1,7 @@
 from databricks.connect import DatabricksSession
 from pyspark.sql import SparkSession
 from dabs_poc import main
+import great_expectations as ge
 
 # Create a new Databricks Connect session. If this fails,
 # check that you have configured Databricks Connect correctly.
@@ -12,3 +13,12 @@ SparkSession.builder.getOrCreate()
 def test_main():
     taxis = main.get_taxis()
     assert taxis.count() > 5
+
+
+def test_tpep_pickup_datetime_not_null():
+    taxis = main.get_taxis()
+    gedf = ge.dataset.SparkDFDataset(taxis)
+    column_name = "tpep_pickup_datetime"
+
+    assert gedf.expect_column_values_to_not_be_null(column_name)['success'] == True, \
+            f"Column '{column_name}' contains null values"
